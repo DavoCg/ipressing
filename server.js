6d0354f195658f4200dfc9e7a07a02a96941b3fb
David Cingala <david.cingala@gmail.com>
2015-10-02 11:24:55 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.use(express.static(path.resolve(__dirname, 'dist')));
+
+app.listen(5678, function(){
+    console.log('server listen ')
+});
